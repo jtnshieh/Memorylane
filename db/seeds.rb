c40ee3a35c1c35edc4aa8f10394c9b045a3d89d1
Mac <jtnshieh@gmail.com>
2017-09-21 17:26:14 -0700

    changed color of link and seeded user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User.destroy.all
+
+user1 = User.create!({full_name: "Betty Lam", username: "betty", password:"123abc"})
+user2 = User.create!({full_name: "Nixon Yiu", username: "nixon", password:"123456"})
+user3 = User.create!({full_name: "Michael Quint", username: "michael", password:"1234567"})
+user4 = User.create!({full_name: "Rebekah Liu", username: "Rebekah", password:"123abcd"})
+user5 = User.create!({full_name: "David Jang", username: "david", password:"123abcde"})
